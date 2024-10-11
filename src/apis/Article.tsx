891f43a989d14b8ebd8021f23cd6224ae2634cbe
hyunhomon <hyunhomon@gmail.com>
2024-10-11 16:32:41 +0900

    fix: export

--- src/apis/Article.tsx
+++ src/apis/Article.tsx
@@ -1,16 +1,16 @@
 import client from "./Client";
 
-const articleGet = async (page: number) => {
+export const articleGet = async (page: number) => {
     const response = await client.get('/article?page=' + page)
     return response.data
 };
 
-const articleSet = async (article: IArticle) => {
+export const articleSet = async (article: IArticle) => {
     const response = await client.post('/article', article)
     return response.data
 };
 
-const articleDel = async (id: number) => {
+export const articleDel = async (id: number) => {
     const response = await client.delete('/article?id=' + id.toString())
     return response.data
 };
--- src/apis/User.tsx
+++ src/apis/User.tsx
@@ -1,16 +1,16 @@
 import client from "./Client";
 
-const userGet = async (email: string) => {
+export const userGet = async (email: string) => {
     const response = await client.get('/user?email=' + email)
     return response.data
 };
 
-const userSet = async (user: IUser) => {
+export const userSet = async (user: IUser) => {
     const response = await client.post('/user', user)
     return response.data
 };
 
-const userDel = async (email: string) => {
+export const userDel = async (email: string) => {
     const response = await client.delete('/user?email=' + email)
     return response.data
 };
